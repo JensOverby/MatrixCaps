57a08cc95c48245c6f2ecbebc5c4ba1b1d5adeda
JensOverby <jens_overby@yahoo.com>
2018-06-20 10:22:55 +0200

    files moved and added opengl stereo project

---
+++ EM-clustering/GMM.py
@@ -0,0 +1,292 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from torch.autograd import Variable
+from scipy.stats import multivariate_normal
+np.cat = np.concatenate
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
+from tqdm import tqdm_notebook as tqdm
+from torch.distributions import Normal
+
+
+torch.__version__
+
+def sample(mu, var, nb_samples=500):
+    """
+    Return a tensor of (nb_samples, features), sampled
+    from the parameterized gaussian.
+    
+    :param mu: torch.Tensor of the means
+    :param var: torch.Tensor of variances (NOTE: zero covars.)
+    """
+    out = []
+    for i in range(nb_samples):
+        out += [
+            torch.normal(mu, var.sqrt()).unsqueeze(0)
+        ]
+    return torch.cat(out, dim=0)
+
+# generate some clusters
+cluster1 = sample(
+    torch.Tensor([2.5, 2.5]),
+    torch.Tensor([1.2, .8])
+)
+
+cluster2 = sample(
+    torch.Tensor([7.5, 7.5]),
+    torch.Tensor([.75, .5])
+)
+
+cluster3 = sample(
+    torch.Tensor([8, 1.5]),
+    torch.Tensor([.6, .8])
+)
+
+def plot_2d_sample(sample):
+    sample_np = sample.numpy()
+    x = sample_np[:, 0]
+    y = sample_np[:, 1]
+    plt.scatter(x, y)
+    
+# create the dummy dataset, by combining the clusters.
+X = torch.cat([cluster1, cluster2, cluster3])
+plot_2d_sample(X)
+
+def get_k_likelihoods(X, mu, var):
+    """
+    Compute the densities of each data point under the parameterised gaussians.
+    
+    :param X: design matrix (examples, features)
+    :param mu: the component means (K, features)
+    :param var: the component variances (K, features)
+    :return: relative likelihoods (K, examples)
+    """
+    
+    if var.data.eq(0).any():
+        raise Exception('variances must be nonzero')
+    
+    # get the trace of the inverse covar. matrix
+    covar_inv = 1. /  var # (K, features)
+    
+    # compute the coefficient
+    det = (2 * np.pi * var).prod(dim=1) # (K)
+    coeff = 1. / det.sqrt() # (K)
+    
+    # tile the design matrix `K` times on the batch dimension 
+    K = mu.size(0)
+    X = X.unsqueeze(0).repeat(K, 1, 1)
+    
+    # calculate the exponent
+    a = (X - mu.unsqueeze(1)) # (K, examples, features)
+    exponent = a ** 2 @ covar_inv.unsqueeze(2)
+    exponent = -0.5 * exponent
+    
+    # compute probability density
+    P = coeff.view(K, 1, 1) * exponent.exp()
+    
+    # remove final singleton dimension and return
+    return P.squeeze(2)
+
+def plotXYZ(XYZ):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.gca(projection='3d')
+    for i in range(XYZ.shape[0]):
+        for j in range(XYZ.shape[1]):
+            xs, ys, zs = XYZ[i,j,:]
+            ax.scatter(xs, ys, zs, c='r', marker='o')
+#     plt.savefig('fig_{}.png'.format(i), dpi=400, bbox_inches='tight')
+    plt.show()
+
+#>>> points.shape
+#torch.Size([10000, 2])
+#>>> P.shape
+#torch.Size([3, 10000])    
+
+def get_density(mu, var, pi, N=50, X_range=(0, 5), Y_range=(0, 5)):
+    """ Get the mesh to compute the density on. """
+    X = np.linspace(*X_range, N)
+    Y = np.linspace(*Y_range, N)
+    X, Y = np.meshgrid(X, Y)
+    
+    # get the design matrix
+    points = np.cat([X.reshape(-1, 1), Y.reshape(-1, 1)], axis=1)
+    points = Variable(torch.from_numpy(points).float())
+    
+    # compute the densities under each mixture
+    P = get_k_likelihoods(points, mu, var)
+
+    # sum the densities to get mixture density
+    Z = torch.sum(P, dim=0).data.numpy().reshape([N, N])
+    
+    return X, Y, Z
+
+def plot_density(X, Y, Z, i=0):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=3, cstride=3, linewidth=1, antialiased=True,
+                    cmap=cm.inferno)
+    cset = ax.contourf(X, Y, Z, zdir='z', offset=-0.15, cmap=cm.inferno)
+
+    # adjust the limits, ticks and view angle
+    ax.set_zlim(-0.15,0.2)
+    ax.set_zticks(np.linspace(0,0.2,5))
+    ax.view_init(27, -21)
+#     plt.savefig('fig_{}.png'.format(i), dpi=400, bbox_inches='tight')
+    plt.show()
+
+def EM_routing(R, V):
+    eps = 1e-6
+    iteration = 10
+    
+    for i in range(iteration):
+        # M-step
+        #R = (R * a_).unsqueeze(-1)
+        R = R.unsqueeze(-1)
+        sum_R = R.sum(1)
+        mu = ((R * V).sum(1) / sum_R).unsqueeze(1)
+        sigma_square = (R * (V - mu) ** 2).sum(1) / sum_R
+
+        #cost = (self.beta_v.view(kaj) + torch.log(sigma_square.sqrt().view(b_C_w)+self.eps)) * sum_R.view(b_C_w)
+        #a = torch.sigmoid(lambda_ * (self.beta_a.view(aag) - cost.sum(-1)))
+        #a = a.view(self.b, -1)
+
+        # E-step
+        if i != iteration - 1:
+            normal = Normal(mu, sigma_square.sqrt().unsqueeze(1))
+            p = torch.exp(normal.log_prob(V+eps))
+            ap = p.prod(-1)
+
+            plot_density(*get_density(mu.squeeze(1), sigma_square, pi, N=100, X_range=(-2, 12), Y_range=(-2, 12)), i=i)
+            #XYZ = torch.stack([V[:,:,0],V[:,:,1],ap], dim=-1)
+            #plotXYZ(XYZ)
+            
+            #ap = a__.unsqueeze(1) * p.sum(-1)
+            R = Variable(ap / ap.sum(0, keepdim=True), requires_grad=False) + eps
+            #R = Variable(ap / ap.sum(-1, keepdim=True), requires_grad=False) + eps
+
+    return mu
+
+
+# training loop
+k = 3
+d = 2
+nb_iters = 1000
+
+data = Variable(X)
+
+# tile the design matrix `K` times on the batch dimension 
+X = data.unsqueeze(0).repeat(k, 1, 1)
+
+
+"""
+Randomly initialize the parameters for `k` gaussians.
+
+data: design matrix (examples, features)
+k: number of gaussians
+var: initial variance
+"""
+
+# choose k points from data to initialize means
+var = 1
+m = data.size(0)
+idxs = Variable(torch.from_numpy(
+    np.random.choice(m, k, replace=False)))
+mu = data[idxs].unsqueeze(1)
+
+# uniform sampling for means and variances
+var = Variable(torch.Tensor(k, d).fill_(var)).unsqueeze(1)
+
+# equal priors
+pi = Variable(torch.Tensor(k).fill_(1)) / k
+
+
+
+
+
+print(mu.size())
+
+prev_cost = float('inf')
+thresh = 1e-4
+eps = 1e-6
+for i in tqdm(range(nb_iters)):
+
+    # E-STEP
+
+    # get the likelihoods p(x|z) under the parameters
+    #p = get_k_likelihoods(data, mu, var)
+
+    normal = Normal(mu, var.sqrt())
+    P = torch.exp(normal.log_prob(X))
+    P = P.prod(-1)
+    
+    #XYZ = torch.stack([X[:,:,0],X[:,:,1],P], dim=-1)
+    #plotXYZ(XYZ)
+
+    # plot!
+    #x,y,z = getXYZ(data, P)
+    #plot_density(x,y,z)
+    plot_density(*get_density(mu.squeeze(1), var.squeeze(1), pi, N=100, X_range=(-2, 12), Y_range=(-2, 12)), i=i)
+    
+    # compute the "responsibilities" p(z|x)
+    # P: the relative likelihood of each data point under each gaussian (K, examples)
+    P_sum = torch.sum(P, dim=0, keepdim=True)
+
+    # p(z|x): (K, examples)
+    gamma = P / (P_sum+eps)
+
+
+    EM_routing(gamma, X)
+    
+    # compute the cost
+
+    """
+    Get the log-likelihood of the data points under the given distribution.
+    P: likelihoods / densities under the distributions. (k, examples)
+    pi: priors (K)
+    """
+    
+    # get weight probability of each point under each k
+    sum_over_k = torch.sum(pi.unsqueeze(1) * P, dim=0)
+    
+    # take log probability over each example `m`
+    sum_over_m = torch.sum(torch.log(sum_over_k + eps))
+    
+    # divide by number of training examples
+    cost = -sum_over_m / P.size(1)
+
+    # check for convergence
+    diff = prev_cost - cost
+    if torch.abs(diff).data[0] < thresh:
+        break
+    prev_cost = cost
+
+
+
+    # M-STEP
+
+    # re-compute parameters
+
+    # compute `N_k` the proxy "number of points" assigned to each distribution.
+    N_k = torch.sum(gamma, dim=1) + eps # (K)
+    N_k = N_k.view(k, 1, 1)
+
+
+    # get the means by taking the weighted combination of points
+    mu = (gamma.unsqueeze(2) * X).sum(1, keepdim=True)
+    #mu = gamma.unsqueeze(1) @ X # (K, 1, features)
+    mu = mu / N_k
+    
+    # compute the diagonal covar. matrix, by taking a weighted combination of
+    # the each point's square distance from the mean
+    A = X - mu
+    var = (gamma.unsqueeze(2) * (A ** 2)).sum(1, keepdim=True)
+    #var = gamma.unsqueeze(1) @ (A ** 2) # (K, 1, features)
+    var = var / N_k
+
+    # recompute the mixing probabilities
+    pi = N_k / N_k.sum()
---
+++ OpenGL_stereo/video_18_simple_light.py
@@ -0,0 +1,304 @@
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import pyrr
+from PIL import Image
+import os
+import random
+import math
+import time
+
+
+class ObjLoader:
+    def __init__(self):
+        self.vert_coords = []
+        self.text_coords = []
+        self.norm_coords = []
+
+        self.vertex_index = []
+        self.texture_index = []
+        self.normal_index = []
+
+        self.model = []
+
+    def load_model(self, file, scale=1.0):
+        for line in open(file, 'r'):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+
+            if values[0] == 'v':
+                mylist = []
+                for value in values[1:4]:
+                    mylist.append(str(float(value)*scale))
+                self.vert_coords.append(mylist)
+                #self.vert_coords.append(values[1:4])
+            if values[0] == 'vt':
+                self.text_coords.append(values[1:3])
+            if values[0] == 'vn':
+                self.norm_coords.append(values[1:4])
+
+            if values[0] == 'f':
+                face_i = []
+                text_i = []
+                norm_i = []
+                for v in values[1:4]:
+                    w = v.split('/')
+                    face_i.append(int(w[0])-1)
+                    text_i.append(int(w[1])-1)
+                    norm_i.append(int(w[2])-1)
+                self.vertex_index.append(face_i)
+                self.texture_index.append(text_i)
+                self.normal_index.append(norm_i)
+
+        self.vertex_index = [y for x in self.vertex_index for y in x]
+        self.texture_index = [y for x in self.texture_index for y in x]
+        self.normal_index = [y for x in self.normal_index for y in x]
+
+        for i in self.vertex_index:
+            self.model.extend(self.vert_coords[i])
+
+        for i in self.texture_index:
+            self.model.extend(self.text_coords[i])
+
+        for i in self.normal_index:
+            self.model.extend(self.norm_coords[i])
+
+        self.model = np.array(self.model, dtype='float32')
+
+def render_to_jpg(format="PNG"):
+    filename = "dump_1.png"
+    os.chdir(r"./dumps")
+    while True:
+        file = ''
+        for file in os.listdir(os.curdir):
+            if file == filename:
+                name, ext = file.split(".")
+                word, number = name.split("_")
+                new_num = int(number) + 1
+                filename = word + "_" + str(new_num) + "." + ext
+                file = filename
+                break
+            else:
+                continue
+        if (file != filename):
+            break
+
+    x, y, width, height = glGetDoublev(GL_VIEWPORT)
+    width, height = int(width), int(height)
+    glPixelStorei(GL_PACK_ALIGNMENT, 1)
+    data = glReadPixels(x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE)
+    image = Image.frombytes("RGB", (width, height), data)
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    image = image.resize((100,100), Image.ANTIALIAS)
+    image.save(filename, format)
+    os.chdir(r"..")
+
+rot_a = 0.0
+rot_b = 0.0
+rotating = False
+pos_x = 0.0
+pos_y = 0.0
+translating = False
+mouse_pos_x = 0
+mouse_pos_y = 0
+make_samples_count = 0
+eye_distance = 0.2
+
+def key_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_F12 and action == glfw.PRESS:
+        global make_samples_count
+        make_samples_count = 100
+        #render_to_jpg()
+    if key == glfw.KEY_ESCAPE:
+        global rot_a
+        global rot_b
+        global pos_x
+        global pos_y
+        rot_a = 0.0
+        rot_b = 0.0
+        pos_x = 0.0
+        pos_y = 0.0
+
+def cursor_pos_callback(window, x, y):
+    global mouse_pos_x
+    global mouse_pos_y
+    if rotating:
+        global rot_a
+        global rot_b
+        rot_a += (mouse_pos_x-x)/100.0
+        rot_b += (mouse_pos_y-y)/100.0
+    elif translating:
+        global pos_x
+        global pos_y
+        pos_x += -(mouse_pos_x-x)/200.0
+        pos_y += (mouse_pos_y-y)/200.0
+    mouse_pos_x = x
+    mouse_pos_y = y
+
+def mouse_button_callback(window, button, action, mods):
+    if button == glfw.MOUSE_BUTTON_LEFT:
+        global rotating
+        rotating = (action == glfw.PRESS)
+    elif button == glfw.MOUSE_BUTTON_RIGHT:
+        global translating
+        translating = (action == glfw.PRESS)
+        if not translating:
+            print ("pos_x=",pos_x)
+            print ("pos_y=",pos_y)
+
+def scroll_callback(window, xOffset, yOffset):
+    print (xOffset, yOffset)
+
+def load_shader(shader_file):
+    shader_source = ""
+    with open(shader_file) as f:
+        shader_source = f.read()
+    f.close()
+    return str.encode(shader_source)
+
+def compile_shader(vs, fs):
+    vert_shader = load_shader(vs)
+    frag_shader = load_shader(fs)
+
+    shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(vert_shader, GL_VERTEX_SHADER),
+                                              OpenGL.GL.shaders.compileShader(frag_shader, GL_FRAGMENT_SHADER))
+    return shader
+
+def window_resize(window, width, height):
+    minimum = min(width,height)
+    glViewport(0, 0, minimum, minimum)
+
+def main():
+
+    global make_samples_count
+
+    # initialize glfw
+    if not glfw.init():
+        return
+
+    w_width, w_height = 800, 800
+
+    #glfw.window_hint(glfw.RESIZABLE, GL_FALSE)
+
+    window = glfw.create_window(w_width, w_height, "My OpenGL window", None, None)
+
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    glfw.set_window_size_callback(window, window_resize)
+    glfw.set_key_callback(window, key_callback)
+    
+    glfw.set_cursor_pos_callback(window, cursor_pos_callback)
+    glfw.set_mouse_button_callback(window, mouse_button_callback)
+    glfw.set_scroll_callback(window, scroll_callback)
+
+    obj = ObjLoader()
+    obj.load_model("Rabbit.obj", scale=0.7)
+
+    texture_offset = len(obj.vertex_index)*12
+    normal_offset = (texture_offset + len(obj.texture_index)*8)
+
+    shader = compile_shader("video_18_vert.c", "video_18_frag.c")
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, obj.model.itemsize * len(obj.model), obj.model, GL_STATIC_DRAW)
+
+    #positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, obj.model.itemsize * 3, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(0)
+    #textures
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, obj.model.itemsize * 2, ctypes.c_void_p(texture_offset))
+    glEnableVertexAttribArray(1)
+    #normals
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, obj.model.itemsize * 3, ctypes.c_void_p(normal_offset))
+    glEnableVertexAttribArray(2)
+
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    # Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    # Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # load image
+    image = Image.open("Rabbit_D.tga")
+    flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = np.array(list(flipped_image.getdata()), np.uint8)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+    glEnable(GL_TEXTURE_2D)
+
+    glUseProgram(shader)
+
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+    glEnable(GL_DEPTH_TEST)
+    #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+    focus_distance = 1.0
+    view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -focus_distance]))
+    projection = pyrr.matrix44.create_perspective_projection_matrix(65.0, w_width / w_height, 0.1, 100.0)
+    model = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, 0.0]))
+
+    view_loc = glGetUniformLocation(shader, "view")
+    proj_loc = glGetUniformLocation(shader, "projection")
+    model_loc = glGetUniformLocation(shader, "model")
+    transform_loc = glGetUniformLocation(shader, "transform")
+    light_loc = glGetUniformLocation(shader, "light")
+
+    glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        if make_samples_count > 0:
+            rot = pyrr.Matrix44.from_x_rotation(random.random()*2*math.pi)
+            rot *= pyrr.Matrix44.from_y_rotation(random.random()*2*math.pi)
+            trans = pyrr.Matrix44.identity(float)
+            trans[3,0] = pos_x #+ random.random()*0.4 - 0.2
+            trans[3,1] = pos_y #+ random.random()*0.2 - 0.1
+            eye_angle = eye_distance/focus_distance
+
+            right_eye_transform = pyrr.Matrix44.from_y_rotation(eye_angle/2)
+            left_eye_transform = pyrr.Matrix44.from_y_rotation(-eye_angle/2)
+            
+            glUniformMatrix4fv(transform_loc, 1, GL_FALSE, right_eye_transform*rot*trans)
+            glUniformMatrix4fv(light_loc, 1, GL_FALSE, rot*trans)
+            glDrawArrays(GL_TRIANGLES, 0, len(obj.vertex_index))
+            glfw.swap_buffers(window)
+            
+            time.sleep(1)
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+            glUniformMatrix4fv(transform_loc, 1, GL_FALSE, left_eye_transform*rot*trans)
+            glUniformMatrix4fv(light_loc, 1, GL_FALSE, rot*trans)
+            glDrawArrays(GL_TRIANGLES, 0, len(obj.vertex_index))
+            glfw.swap_buffers(window)
+            time.sleep(1)
+            
+            make_samples_count -= 1
+           
+        else:
+            rot = pyrr.Matrix44.from_x_rotation(rot_b)
+            rot *= pyrr.Matrix44.from_y_rotation(rot_a)
+            trans = pyrr.Matrix44.identity(float)
+            trans[3,0] = pos_x
+            trans[3,1] = pos_y
+    
+            glUniformMatrix4fv(transform_loc, 1, GL_FALSE, rot*trans)
+            glUniformMatrix4fv(light_loc, 1, GL_FALSE, rot*trans)
+            glDrawArrays(GL_TRIANGLES, 0, len(obj.vertex_index))
+            glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
